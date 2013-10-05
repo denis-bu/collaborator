82f7a669abec3ce71731b7a478ad7f8fbbdc71c8
Denis Burenko <burenkod@gmail.com>
2013-10-05 11:43:13 +0400

    add messaging UI

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,73 @@
+function make_flash(css_class, content) {
+  $("<div class='" + css_class +"'></div>").html(content).purr();
+}
+
+function flash_success(content) {
+  make_flash("alert alert-success", content);
+}
+
+function flash_info(content) {
+  make_flash("alert alert-info", content);
+}
+
+function flash_warning(content) {
+  make_flash("alert", content);
+}
+
+function flash_error(content) {
+  make_flash("alert alert-error", content);
+}
+
+
+function ConstInterval(timeout) {
+  this.timeout = timeout;
+}
+
+ConstInterval.prototype.reset = function() { };
+ConstInterval.prototype.next = function() { return this.timeout; };
+
+
+function DampedInterval(start, end, rate) {
+  this.start = start;
+  this.end = end;
+  this.rate = rate;
+
+  this.reset();
+}
+
+DampedInterval.prototype.reset = function() {
+  this.current = this.start / this.rate;
+};
+
+DampedInterval.prototype.next = function() {
+  this.current *= this.rate;
+  if (this.current > this.end)
+    this.current = this.end;
+
+  return this.current;
+};
+
+
+function Scheduler(callback, intervalGenerator) {
+  var that = this;
+
+  var timerCallback = function() {
+    if (that.timerId)
+      callback();
+    that.timerId = window.setTimeout(timerCallback, intervalGenerator.next());
+  };
+
+  timerCallback();
+
+  this.pulse = function() {
+    // Clear timeout.
+    if (that.timerId) {
+      window.clearTimeout(that.timerId);
+      delete that.timerId;
+    }
+    // Restart generator.
+    intervalGenerator.reset();
+
+    timerCallback();
+  };
+}
---
+++ app/assets/javascripts/messaging_ui.js
@@ -0,0 +1,82 @@
+function MessagePane(element) {
+  this.$elementPane = jQuery(element);
+}
+
+MessagePane.prototype.prependMessage = function(message) {
+  var messageText = message.author + '(' + message.timestamp + '): ' + message.content;
+  jQuery('<li />').text(messageText).prependTo(this.$elementPane);
+};
+
+MessagePane.prototype.appendMessage = function(message) {
+  var messageText = message.author + '(' + message.timestamp + '): ' + message.content;
+  jQuery('<li />').text(messageText).appendTo(this.$elementPane);
+};
+
+
+function Messenger(element, url) {
+  this.fetcher = new MessageFetcher(url);
+  this.pane = new MessagePane(element);
+
+  this.sequence_begin = 4294967295;  // TODO: fix magic number.
+  this.sequence_end = 0;
+
+  this.fetchDefault();
+
+  var that = this;
+  var intervalGenerator = new DampedInterval(1000, 30000, 1.25);
+  this.scheduler = new Scheduler(function() { that.fetchLatest(); }, intervalGenerator);
+
+}
+
+Messenger.prototype.fetchDefault = function() {
+  var that = this;
+  this.fetcher.fetchDefault(function(messages) {
+    that.addMessagesForward(messages)
+  });
+};
+
+Messenger.prototype.fetchLatest = function() {
+  var that = this;
+  this.fetcher.fetchLatest(function(messages) {
+    that.addMessagesForward(messages)
+  }, this.sequence_end);
+};
+
+Messenger.prototype.fetchBackward = function(limit) {
+  var that = this;
+  this.fetcher.fetchBackward(function(messages) {
+    that.addMessagesBackward(messages)
+  }, this.sequence_begin, limit);
+};
+
+Messenger.prototype.addMessagesForward = function(messages) {
+  for (var i = 0; i < messages.length; i++) {
+    this.pane.appendMessage(messages[i]);
+
+    if (this.sequence_end < messages[i].sequence)
+      this.sequence_end = messages[i].sequence;
+
+    if (this.sequence_begin > messages[i].sequence)
+      this.sequence_begin = messages[i].sequence;
+  }
+
+  //flash_success('Success in fetcher!');
+};
+
+Messenger.prototype.addMessagesBackward = function(messages) {
+  for (var i = 0; i < messages.length; i++) {
+    this.pane.prependMessage(messages[i]);
+
+    if (this.sequence_end < messages[i].sequence)
+      this.sequence_end = messages[i].sequence;
+
+    if (this.sequence_begin > messages[i].sequence)
+      this.sequence_begin = messages[i].sequence;
+  }
+
+  //flash_success('Success in fetcher!');
+};
+
+Messenger.prototype.pulse = function() {
+  this.scheduler.pulse();
+};
